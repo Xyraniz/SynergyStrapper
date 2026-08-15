61844fe9bf9b7e8d791d8089505861c8da836826
Xyraniz <1.09302452e+08+Xyraniz@users.noreply.github.com>
2026-08-15 19:48:04 +0000

    fix: import BitmapImage for bootstrapper dialog

--- SynergyStrapper/UI/ViewModels/Bootstrapper/BootstrapperDialogViewModel.cs
+++ SynergyStrapper/UI/ViewModels/Bootstrapper/BootstrapperDialogViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Shell;
 
 using CommunityToolkit.Mvvm.Input;
