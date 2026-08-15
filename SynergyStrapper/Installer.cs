4391c36f71d55aa1b3335b8c061686761a07cb41
Xyraniz <1.09302452e+08+Xyraniz@users.noreply.github.com>
2026-08-15 07:36:43 +0000

    fix: open current release notes after upgrade

--- SynergyStrapper/Installer.cs
+++ SynergyStrapper/Installer.cs
@@ -13,7 +13,7 @@ namespace SynergyStrapper
         /// Which version's release notes to open
         /// Leave blank to use the current version
         /// </summary>
-        private const string ForcedReleaseNotesVersion = "2.11.2";
+        private const string ForcedReleaseNotesVersion = "";
 
         private static string DesktopShortcut => Path.Combine(Paths.Desktop, $"{App.ProjectName}.lnk");
 
